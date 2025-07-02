41a404ef2545698bd86a2966da46d46c16c9453f
Ali <ali.ezad77@gmail.com>
2025-07-02 17:10:32 +0330

    more initial configuration

---
+++ lib/flutter_chapar_interview.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chapar_interview/features/personal_info/presentation/page/personal_info_page.dart';
+import 'package:flutter_chapar_interview/generated/l10n.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class FlutterChaparInterview extends StatelessWidget {
+  const FlutterChaparInterview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chapar Interview',
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('en')],
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        appBarTheme: const AppBarTheme(centerTitle: true),
+      ),
+      home: const PersonalInfoPage(),
+    );
+  }
+}
