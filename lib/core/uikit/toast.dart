2d5de75f5bdb830b191b65014af29bcf83095e28
Ali <ali.ezad77@gmail.com>
2025-07-02 21:41:38 +0330

    add toast to uikit

---
+++ lib/core/uikit/toast.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+showErrorToast(BuildContext context, String message) {
+  ScaffoldMessenger.of(context)
+    ..hideCurrentSnackBar()
+    ..showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Theme.of(context).colorScheme.error,
+      ),
+    );
+}
+
+showSuccessToast(BuildContext context, String message) {
+  ScaffoldMessenger.of(context)
+    ..hideCurrentSnackBar()
+    ..showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+}
